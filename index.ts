69c8aacdcbad20e324c621401ae61b4eb8b537b4
James Tedy <james.tedy@ymail.com>
2020-11-11 19:12:56 +1100

    Make new types and interface
    - Make new interface for  KYCCheck function

--- index.ts
+++ index.ts
@@ -2,4 +2,19 @@ import * as dotenv from "dotenv";
 
 const config = dotenv.config({ path: "./.env" });
 
-console.log("Hello World!");
+type stateName = "NSW" | "QLD" | "ACT" | "SA" | "TAS" | "VIC" | "WA" | "NT";
+type kycResult = { kyrcResult: boolean };
+
+interface KYCCheck {
+  (
+    dob: string,
+    fName: string,
+    lName: string,
+    licenseNumb: string,
+    state: stateName,
+    expiryDate?: string,
+    mName?: string
+  ): void | kycResult;
+}
+
+const kycCheck = () => {};
