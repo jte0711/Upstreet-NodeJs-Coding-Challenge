e9e1a4051cf6cb43820550d4d15bc75011713b1e
James Tedy <james.tedy@ymail.com>
2020-11-12 11:39:58 +1100

    Add input check to the function
    - Add lengthCheck to check if name length within the maximum limit
    - Add formatCheck to check if date given is within the correct format and a valid date

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv";
+import * as axios from "axios";
 
+const ax = axios.default;
 const config = dotenv.config({ path: "./.env" });
 
 type stateName = "NSW" | "QLD" | "ACT" | "SA" | "TAS" | "VIC" | "WA" | "NT";
-type kycResult = { kyrcResult: boolean };
+type kycResult = { kyrcResult?: boolean; code?: string; message?: string };
 
 interface KYCCheck {
   (
@@ -14,7 +16,98 @@ interface KYCCheck {
     state: stateName,
     expiryDate?: string,
     mName?: string
-  ): void | kycResult;
+  ): void | Promise<kycResult>;
 }
 
-const kycCheck = () => {};
+const lengthCheck = (word) => {
+  if (word.length > 100) {
+    throw new Error(`"${word}" exceeds 100 characters limit`);
+  }
+};
+
+const formatCheck = (date) => {
+  const correctFormat: RegExp = /^\d{4}[-]\d{2}[-]\d{2}$/;
+  const correctDate: RegExp = /^\d{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/;
+  if (!correctFormat.test(date)) {
+    throw new Error(`"${date}" must be in YYYY-MM-DD format`);
+  }
+  if (!correctDate.test(date)) {
+    throw new Error(`${date}" must be a valid date`);
+  }
+};
+
+const kycCheck: KYCCheck = async (
+  dob,
+  fName,
+  lName,
+  licenseNumb,
+  state,
+  expDate,
+  mName
+) => {
+  try {
+    // Check if fname, mname, and lastname within 100 char limit
+    lengthCheck(fName);
+    lengthCheck(lName);
+    mName ? lengthCheck(mName) : null;
+
+    // Check if dob and expiry date within format
+    formatCheck(dob);
+    expDate ? formatCheck(expDate) : null;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  let data = {
+    birthDate: dob,
+    givenName: fName,
+    middleName: mName,
+    familyName: lName,
+    licenceNumber: licenseNumb,
+    stateOfIssue: state,
+    expiryDate: expDate,
+  };
+
+  // let config = {
+  //   headers: {
+  //     Authorization: process.env.API_KEY,
+  //     "Access-Control-Allow-Origin": "*",
+  //   },
+  // };
+
+  // // POST data
+  // const response = await ax
+  //   .post(process.env.API_ENDPOINT, data, config)
+  //   .then((res) => {
+  //     return res;
+  //   })
+  //   .catch((e) => {
+  //     console.log("THIS IS ERROR \n", e);
+  //   });
+
+  // // console.log("This is response \n", response);
+  // const resCode = response["verificationResponseCode"];
+
+  // if (resCode === "Y") {
+  //   return { kyrcResult: true };
+  // } else if (resCode === "N") {
+  //   return { kyrcResult: false };
+  // } else {
+  //   return {
+  //     code: resCode === "D" ? "D" : "S",
+  //     message: resCode === "D" ? "Document Error" : "Server Error",
+  //   };
+  // }
+  return { kyrcResult: true };
+};
+
+kycCheck(
+  "1999-02-21",
+  "Johnny",
+  "Goodman",
+  "94977000",
+  "VIC",
+  "2020-01-01",
+  "Robert"
+);
